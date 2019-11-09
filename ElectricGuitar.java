90b73c26b63b109170189573beb2c1492d6bface
Vlad Verbytskyi <vlad.verbytskyi2019@gmail.com>
2019-11-10 00:18:00 +0200

    I add new task by my book

---
+++ ElectricGuitar.java
@@ -0,0 +1,15 @@
+public class ElectricGuitar {
+           String brand;
+        int numOfPickups;
+        boolean rockStarUsesIt;
+
+        String getBrand() {
+            return brand;
+        }
+
+        void setBrand(String aBrand) {
+            brand = aBrand;
+        }
+    }
+
+
