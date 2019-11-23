29ca58bfed0647a5c1e27ad52a4b1c25b2270e8e
Vlad Verbytskyi <vlad.verbytskyi2019@gmail.com>
2019-11-23 12:11:31 +0200

    2 party DingeonTest

---
+++ DungeonGame.java
@@ -0,0 +1,21 @@
+package Pool497;
+
+import java.io.*;
+
+public class DungeonGame implements Serializable {
+    public int x = 3;
+    transient long y = 4;
+    private  short z = 5;
+
+    int getX() {
+        return x;
+    }
+
+    long getY() {
+        return y;
+    }
+
+    short getZ() {
+        return z;
+    }
+}
