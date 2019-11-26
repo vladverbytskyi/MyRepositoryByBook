48a6a3eedb4fafb408c917a56777cfd52e00bef7
Vlad Verbytskyi <vlad.verbytskyi2019@gmail.com>
2019-11-26 19:33:06 +0200

    Thread

---
+++ TestThreads.java
@@ -0,0 +1,57 @@
+package MagniteCode546;
+
+public class TestThreads {
+    public static void main(String [] args) {
+        ThreadOne t1 = new ThreadOne();
+        ThreadTwo t2 = new ThreadTwo();
+        Thread one = new Thread(t1);
+        Thread two = new Thread(t2);
+        one.start();
+        two.start();
+
+    }
+}
+class Accum {
+    private static Accum a = new Accum();
+    private int counter = 0;
+
+    private Accum() { }
+
+    public static Accum getAccum() {
+        return a;
+    }
+    public void updateCounter (int add) {
+        counter += add;
+    }
+    public int getCount() {
+        return counter;
+    }
+}
+
+
+    class ThreadOne implements Runnable {
+    Accum a = Accum.getAccum();
+        public void run() {
+            for (int x = 0; x < 98; x++) {
+                a.updateCounter(1000);
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException ex) {
+
+                }
+            }
+            System.out.println("Один " + a.getCount());
+        }
+}
+
+    class ThreadTwo implements Runnable {
+    Accum a = Accum.getAccum();
+        public void run() {
+            for (int x = 0; x < 99; x++) {
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException ex) {}
+            }
+            System.out.println("Два " + a.getCount());
+        }
+}
