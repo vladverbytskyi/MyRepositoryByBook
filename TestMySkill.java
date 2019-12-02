b28925dc1dd410585c9b0e3aeae2535dc1bff110
Vlad Verbytskyi <vlad.verbytskyi2019@gmail.com>
2019-12-02 21:27:47 +0200

    Myfirst task from my cursor

---
+++ TestMySkill.java
@@ -0,0 +1,91 @@
+package FirstHomeWork;
+
+public class TestMySkill {
+
+    static byte b;
+    static short s;
+    static long l;
+    static double d;
+    static float f;
+    static int i;
+
+    public static void main (String [] args) {
+        i = 33564;
+        int addInt = i + i;
+        int subInt = i - i;
+        int divInt = i / i;
+        int multInt = i * i;
+
+        System.out.println("Addition " + addInt);
+        System.out.println("Subtraction " + subInt);
+        System.out.println("Division " + divInt);
+        System.out.println("Multiplication " + multInt);
+        System.out.println(" ");
+
+        b = 9;
+        int addByte = b + b;
+        int subByte = b - b;
+        int divByte = b / b;
+        int multByte = b * b;
+
+        System.out.println("Addition " + addByte);
+        System.out.println("Subtraction " + subByte);
+        System.out.println("Division " + divByte);
+        System.out.println("Multiplication " + multByte);
+        System.out.println(" ");
+
+        s = 526;
+        int addShort = s + s;
+        int subShort = s - s;
+        int divShort = s / s;
+        int multShort = s * s;
+
+        System.out.println("Addition " + addShort);
+        System.out.println("Subtraction " + subShort);
+        System.out.println("Division " + divShort);
+        System.out.println("Multiplication " + multShort);
+        System.out.println(" ");
+
+        l = 854775807L;
+        long addLong = l + l;
+        long subLong = l - l;
+        long divLong = l / l;
+        long multLong = l * l;
+
+        System.out.println("Addition " + addLong);
+        System.out.println("Subtraction " + subLong);
+        System.out.println("Division " + divLong);
+        System.out.println("Multiplication " + multLong);
+        System.out.println(" ");
+
+        d = 4.62351653;
+        double addDouble = d + d;
+        double subDouble = d - d;
+        double divDouble = d / d;
+        double multDouble = d *d;
+
+        System.out.println("Addition " + addDouble);
+        System.out.println("Subtraction " + subDouble);
+        System.out.println("Division " + divDouble);
+        System.out.println("Multiplication " + multDouble);
+        System.out.println(" ");
+
+        f = 5.656565f;
+        float addFloat = f + f;
+        float subFloat = f - f;
+        float divFloat = f / f;
+        float multFloat = f * f;
+
+        System.out.println("Addition " + addFloat);
+        System.out.println("Subtraction " + subFloat);
+        System.out.println("Division " + divFloat);
+        System.out.println("Multiplication " + multFloat);
+
+
+
+
+
+
+    }
+
+}
