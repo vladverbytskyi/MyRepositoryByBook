f4ee0ae003018099112657d3b07692e338a541a9
Vlad Verbytskyi <vlad.verbytskyi2019@gmail.com>
2019-12-11 17:48:16 +0200

    I add new file CarAndCircleApplication.java

---
+++ CarAndCircleApplication.java
@@ -0,0 +1,15 @@
+package ThirdHomework;
+
+public class CarAndCircleApplication {
+    public static void main(String[] args) {
+       new CountObject().countObject();
+       new Circle(1).circleArea();
+       
+        Car car = new Car();
+        car.startEngine();
+        car.startCar();
+        car.stopEngine();
+
+
+    }
+}
