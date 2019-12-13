f2f995299e9112a15b6601a41683552ce01bde36
Vlad Verbytskyi <vlad.verbytskyi2019@gmail.com>
2019-12-13 19:02:33 +0200

    I add interfaces in my hw

---
+++ hw4/interfaces/AreaMeasurable.java
@@ -0,0 +1,5 @@
+package interfaces;
+
+public interface AreaMeasurable {
+    double calculateArea();
+}
---
+++ hw4/interfaces/VolumeMeasurable.java
@@ -0,0 +1,5 @@
+package interfaces;
+
+public interface VolumeMeasurable {
+    double calculateVolume();
+}
