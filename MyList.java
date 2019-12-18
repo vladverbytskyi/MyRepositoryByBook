d8cd404f7bed49296313df8f619c9cfbcf356704
Vlad Verbytskyi <vlad.verbytskyi2019@gmail.com>
2019-12-19 00:04:16 +0200

    My hw5

---
+++ MyList.java
@@ -0,0 +1,26 @@
+import org.jetbrains.annotations.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class MyList<T extends Number & Comparable <? super T>> {
+    private List<T> list = new ArrayList<>();
+    public void addArgument(T argument) {
+        this.list.add(argument);
+    }
+    public List<T> getList() {
+        return list;
+    }
+    public double getMax(@NotNull List<T> list) {
+        return list
+                .stream()
+                .mapToDouble(Number::doubleValue)
+                .max().orElseThrow(NoSuchElementException::new);
+    }
+    public double getMin(@NotNull List<T> list) {
+        return list
+                .stream()
+                .mapToDouble(Number::doubleValue)
+                .min().orElseThrow(NoSuchElementException::new);
+    }
+}
