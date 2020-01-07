d2adb67f08cca8c9999da0590ce5e9a082037691
Vlad Verbytskyi <vlad.verbytskyi2019@gmail.com>
2020-01-07 14:39:20 +0200

    added my homework 10

---
+++ hw10/Main.java
@@ -0,0 +1,9 @@
+package com.cursor.hw10;
+
+import com.cursor.hw10.controller.AppExecutor;
+
+public class Main {
+    public static void main(String[] args) {
+        new AppExecutor().runProgram();
+    }
+}
---
+++ hw10/controller/AppExecutor.java
@@ -0,0 +1,9 @@
+package com.cursor.hw10.controller;
+
+import com.cursor.hw10.task1.NutritionFacts;
+
+public class AppExecutor {
+    public void runProgram() {
+        NutritionFacts nutritionFacts = new NutritionFacts(252, 4455, 145, 0, 44, 8884);
+    }
+}
